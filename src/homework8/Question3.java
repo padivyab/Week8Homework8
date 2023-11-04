9d6f6fdc9a2136f68b87f1355687f2e5d6fec494
padivyab <divyapatel.dp908@gmail.com>
2023-11-03 23:30:16 -0400

    data created

--- src/homework8/Question3.java
+++ src/homework8/Question3.java
@@ -1,18 +1,39 @@
 package homework8;
 
-import java.util.Arrays;
+
 
 public class Question3 {
 
     public static void main(String[] args) {
 
-        java.util.Scanner input = new java.util.Scanner(System.in);
+        String[][] CountryAndCapital = {{"United Kingdom","London"},
+                                        {"Australia","Canberra"},
+                                        {"Canada","Ottawa"},
+                                        {"NewZealand","Wellington"},
+                                        {"USA","Washington D.C"}};
 
-        String[][] statesAndCapitols = {
-                {"Alabama","Alaska","Arizona"},{"Arkansas","California","Colorado"},{"Connecticut","Delaware","Florida","Georgia"}};
+        /*String [][] stateAndcapital2 = new String[5][2];
+        stateAndcapital2[0][0] = "Alabama";
+        stateAndcapital2[0][1] = "Alaska";
+        stateAndcapital2[1][0] = "Arkansas";
+        stateAndcapital2[1][1] = "California";
+        stateAndcapital2[2][0] = "Connecticut";
+        stateAndcapital2[2][1] = "Delaware";
+        stateAndcapital2[3][0] = "Florida";
+        stateAndcapital2[3][1] = "Georgia";
+        stateAndcapital2[4][0] = "a";
+        stateAndcapital2[4][1] = "b";*/
 
-        System.out.println(Arrays.toString(statesAndCapitols));
-
+        for (int i=0;i < 5; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                System.out.println(CountryAndCapital[i][j]);
+            }}
+        /*for (int i=0;i<2;i++)
+        {
+            System.out.println(statesAndCapitals[i][i]);
+        }*/
 
     }
 }
